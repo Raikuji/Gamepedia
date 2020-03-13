2213e281046584f71852331fd4e558878d15b06e
lucasburte <lucas.burte@hotmail.fr>
2020-03-13 11:31:01 +0100

    pushed

--- TD3/scripts/Main.php
+++ TD3/scripts/Main.php
@@ -4,6 +4,7 @@ require_once '../vendor/autoload.php';
 //require_once '../src/AppConf.php';
 
 use games\q3;
+use \Illuminate\Database\Capsule\Manager as DB;
 
 $app = new games\AppConf();
 $app->addDbConf("../config/conf.ini");
@@ -11,3 +12,4 @@ $q3 = new q3();
 $q3->whereQuery1();
 $q3->whereQuery2();
 $q3->whereQuery3();
+DB::getQueryLog();
--- TD3/src/AppConf.php
+++ TD3/src/AppConf.php
@@ -2,12 +2,13 @@
 
 namespace games;
 
-use \Illuminate\Database\Capsule\Manager ;
+use \Illuminate\Database\Capsule\Manager as DB;
 
 class AppConf{
     public static function addDbConf($file){
         $conf = parse_ini_file($file);
-        $db = new Manager();
+        $db = new DB();
+        DB::enableQueryLog();
         $db->addConnection($conf);
         $db->setAsGlobal();
         $db->bootEloquent();
